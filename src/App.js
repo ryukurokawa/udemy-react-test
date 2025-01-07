ddcf86b491ef59844fe49766928c4b4776399ee5
ryukurokawa <setsuna1228@gmail.com>
2025-01-07 16:30:02 +0900

    練習３

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const App = () => {
   const profiles = [
     { name: 'Taro', age: 10 },
     { name: 'Hanako', age: 5 },
+    { name: 'NoName' },
   ];
   return (
     <div>
@@ -22,7 +24,9 @@ const User = (props) => {
   );
 };
 
-User.defaultProps = {
-  age: 1,
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
 };
+
 export default App;
