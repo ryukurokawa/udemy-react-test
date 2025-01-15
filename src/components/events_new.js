d5606f02c38c8ae12f0b4a0dac98f365f3b7b249
ryukurokawa <setsuna1228@gmail.com>
2025-01-15 13:02:58 +0900

    リアクトアプリ完成

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 import { postEvent } from '../actions';
 
@@ -21,10 +23,14 @@ class EventsNew extends Component {
     } = Field;
 
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
     );
   }
 
@@ -35,6 +41,7 @@ class EventsNew extends Component {
 
   render() {
     const { handleSubmit, pristine, submitting, invalid } = this.props;
+    const style = { margin: 12 };
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div>
@@ -46,7 +53,6 @@ class EventsNew extends Component {
           />
         </div>
         <div>
-          {' '}
           <Field
             label="Body"
             name="body"
@@ -54,15 +60,17 @@ class EventsNew extends Component {
             component={this.renderField}
           />
         </div>
-
-        <div>
-          <input
-            type="submit"
-            value="Submit"
-            disabled={pristine || submitting || invalid}
-          />
-          <Link to="/">Cancel</Link>
-        </div>
+        <RaisedButton
+          label="Submit"
+          type="submit"
+          style={style}
+          disabled={pristine || submitting || invalid}
+        />
+        <RaisedButton
+          label="Cancel"
+          style={style}
+          containerElement={<Link to="/" />}
+        />
       </form>
     );
   }
